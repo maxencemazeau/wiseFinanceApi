bd94474fe1ead03eee17676a554c058b0d5cf75d
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-08-21 19:50:10 +0200

    Login get

--- Controller/UsersController.js
+++ Controller/UsersController.js
@@ -6,5 +6,11 @@ const getUsers = async () => {
     return rows;
 }
 
+const UsersLogin = async(username, password) => {
+    const query = await db.query('SELECT username, password FROM Users WHERE username = ? and password = ?', [username, password]);
+    const rows = query[0];
+    return rows;
+}
 
-module.exports = getUsers;
+
+module.exports = { getUsers : getUsers, UsersLogin : UsersLogin};
--- Routes/UsersRoutes.js
+++ Routes/UsersRoutes.js
@@ -1,11 +1,16 @@
 const UsersController = require('../Controller/UsersController');
-const getUsers = require('../Controller/UsersController');
 
 const usersRoutes = (fastify, options, done) => {
     fastify.get('/users', async (req, res) => {
-            const users = await getUsers();
+            const users = await UsersController.getUsers();
             res.send(users)
         
+    })
+
+    fastify.get('/login', async(req, res) => {
+        const { username, password } = req.query;
+        const userLogin = await UsersController.UsersLogin(username, password);
+        res.send(userLogin);
     })
 
     done();
